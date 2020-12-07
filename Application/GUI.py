ec4c8c7646027cc4e9a7aba9241f32d100779e0e
--1stRayos <--emc1140@hotmail.com>
2020-12-07 15:26:55 -0600

    Fixed mysql deployment issue

--- Application/GUI.py
+++ Application/GUI.py
@@ -1,20 +1,79 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget,  QPushButton, QTextEdit, QTableView, QStatusBar, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget,  QPushButton, QLineEdit, QLabel, QTextEdit, QFormLayout, QTableView, QStatusBar, QMessageBox
 from PyQt5.QtCore import Qt, QAbstractTableModel, QVariant
 import mysql.connector as sql
 
-class App(QMainWindow):
-    
+class App(QWidget):
+
     def __init__(self):
         
         super(App, self).__init__()
-        self.setWindowTitle('Application')
+        self.setWindowTitle('Login')
         self.configure_gui()
         self.create_widgets()
+        self.show()
+
+    def configure_gui(self):
+        
+        self.setMinimumSize(300, 200)
+        self.layout = QVBoxLayout(self)
+        self.label = QLabel('Enter credentials')
+        self.label.setAlignment(Qt.AlignCenter)
+        self.form = QFormLayout()
+
+    def create_widgets(self):
+        
+        for text in [
+            'Hostname:', 'Database:', 'Username:', 'Password:'
+            ]:
+            widget = QLineEdit(self)
+            widget.returnPressed.connect(self.check_login)
+            self.form.addRow(text, widget)
+        else:
+            button = QPushButton('Submit')
+            button.pressed.connect(self.check_login)
+            self.form.addWidget(button)
+
+        self.layout.addWidget(self.label)
+        self.layout.addLayout(self.form)
+
+    def check_login(self, event=None):
+        
+        try:
+            credentials = {
+                'host': self.form.itemAt(1).widget().text(),
+                'database': self.form.itemAt(3).widget().text(),
+                'user': self.form.itemAt(5).widget().text(),
+                'password': self.form.itemAt(7).widget().text()
+                }
+            Main(credentials)
+            self.close()
+
+        except:
+            QMessageBox().warning(
+                self, 'Wrong credentials', 'You have entered the wrong credentials'
+                )
+
+    def keyPressEvent(self, event):
+    
+        key_press = event.key()
+        modifiers = event.modifiers()
+        alt = modifiers == Qt.AltModifier
+
+        if key_press == Qt.Key_Escape: self.close()
+
+class Main(QMainWindow):
+    
+    def __init__(self, credentials):
+        
+        super(Main, self).__init__()
+        self.setWindowTitle('Application')
+        self.configure_gui()
+        self.create_widgets(credentials)
         self.showMaximized()
 
     def configure_gui(self):
 
-        self.center = QWidget()
+        self.center = QWidget(self)
         self.layout = QVBoxLayout()
         self.center.setLayout(self.layout)
         self.setCentralWidget(self.center)
@@ -23,9 +82,9 @@ class App(QMainWindow):
         self.setStatusBar(self.statusbar)
         self.statusbar.setFixedHeight(25)
 
-    def create_widgets(self):
+    def create_widgets(self, credentials):
         
-        self.connect = MySql(self)
+        self.connect = MySql(self, credentials)
         self.textfield = QTextEdit(self)
         self.textfield.setMaximumHeight(250)
         self.textfield.setPlaceholderText('Enter query')
@@ -61,10 +120,10 @@ class App(QMainWindow):
 
 class MySql(object):
     
-    def __init__(self, parent):
+    def __init__(self, parent, creds):
 
         self.parent = parent
-        self.DATAB = sql.connect(option_files='credentials.ini')
+        self.DATAB = sql.connect(**creds)
         self.CURSOR = self.DATAB.cursor(buffered=True)
 
     def execute(self, statement, arguments=None, many=0, commit=0, fetch=0):
@@ -137,9 +196,10 @@ class Model(QAbstractTableModel):
             return str(self.rows[index.row()][index.column()])
 
         return QVariant()
-    
+
 if __name__ == '__main__':
 
     Qapp = QApplication([])
     app = App()
+
     Qapp.exec_()
